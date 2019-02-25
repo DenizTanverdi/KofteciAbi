c7c05bf2e212e5f23b17abcdd61abf7e08727a60
semvs <semvs@ayvansaray.edu.tr>
2019-02-25 12:31:17 +0300

    Urun Ekleme

---
+++ WebMvc/Controllers/UrunlerController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Urunler
+        public ActionResult Index()
+        {
+            var urunler = db.Urunler.Include(u => u.Student);
+            return View(urunler.ToList());
+        }
+
+        // GET: Urunler/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urunler);
+        }
+
+        // GET: Urunler/Create
+        public ActionResult Create()
+        {
+            ViewBag.kategoriId = new SelectList(db.Urunler, "Id", "UrunAdi");
+            return View();
+        }
+
+        // POST: Urunler/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,kategoriId,UrunAdi,Adet,Fiyat,Url,OlusturmaTarihi,GuncellemeTarihi")] Urunler urunler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Urunler.Add(urunler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.kategoriId = new SelectList(db.Urunler, "Id", "UrunAdi", urunler.kategoriId);
+            return View(urunler);
+        }
+
+        // GET: Urunler/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.kategoriId = new SelectList(db.Urunler, "Id", "UrunAdi", urunler.kategoriId);
+            return View(urunler);
+        }
+
+        // POST: Urunler/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,kategoriId,UrunAdi,Adet,Fiyat,Url,OlusturmaTarihi,GuncellemeTarihi")] Urunler urunler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(urunler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.kategoriId = new SelectList(db.Urunler, "Id", "UrunAdi", urunler.kategoriId);
+            return View(urunler);
+        }
+
+        // GET: Urunler/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = db.Urunler.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(urunler);
+        }
+
+        // POST: Urunler/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Urunler urunler = db.Urunler.Find(id);
+            db.Urunler.Remove(urunler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
