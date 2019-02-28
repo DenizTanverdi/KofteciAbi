c7d466552ee8e4eee716cc4785f4d19c1bed1f4a
semvs <semvs@ayvansaray.edu.tr>
2019-02-28 12:58:44 +0300

    DropList

--- WebMvc/Controllers/LoginController.cs
+++ WebMvc/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using WebMvc.Models;
 
@@ -33,6 +34,30 @@ namespace WebMvc.Controllers
             var list = db.Urunler.ToList();
 
             return PartialView("_urunPartialView", list);
+        }
+        public PartialViewResult KategoriGetir()
+        {
+            var list = db.Kategori.ToList();
+
+            return PartialView("_kategoriPartialView", list);
+        }
+        public PartialViewResult editUrun(int? id)
+        {
+            List<SelectListItem> kategoriler = new List<SelectListItem>();
+            //foreach ile db.Categories deki kategorileri listemize ekliyoruz
+            foreach (var item in db.Kategori.ToList())
+            {   //Text = Görünen kısımdır. Kategori ismini yazdıyoruz
+                //Value = Değer kısmıdır.ID değerini atıyoruz
+                kategoriler.Add(new SelectListItem { Text = item.KategoriAdi, Value = item.Id.ToString() });
+            }
+            //Dinamik bir yapı oluşturup kategoriler list mizi view mize göndereceğiz
+            //bunun için viewbag kullanıyorum
+            ViewBag.Kategoriler = kategoriler;
+
+
+            var list = db.Urunler.Find(id);
+
+            return PartialView("_urunEditPartialView", list);
         }
     }
 }
